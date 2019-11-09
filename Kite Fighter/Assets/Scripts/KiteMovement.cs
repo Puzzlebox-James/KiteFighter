208631569eac2c421c76b025f9cc5e55a7001dc3
James <40773458+Puzzlebox-James@users.noreply.github.com>
2019-11-08 17:13:02 -0700

    Kite Movement workin
    Sigmoid function implementation

--- Kite Fighter/Assets/Scripts/KiteMovement.cs
+++ Kite Fighter/Assets/Scripts/KiteMovement.cs
@@ -47,6 +47,40 @@ public class KiteMovement : MonoBehaviour
         // The velocity vector is used to combine the kiteships various influence vectors into its final movement vector.
         Vector3 velocity = transform.position;
 
+        // Create a rotation vector based off stick position difference and rotate based off it.
+        float stickOffset = (leftStickVector.y - rightStickVector.y) * 2;
+        Vector3 rotationVector = new Vector3(0, 0, -stickOffset);
+        transform.Rotate(rotationVector, Space.World);
+
+        // We need to apply 'upwards' velocity to the kiteship, with increasing velocity the more upside down it is.
+        // We do this by getting the rotation about the z axis, squashing it into a float between 0 - 3, sigmoid style
+        // Then apply that as a scalar to the local up varable of the kiteship.
+
+        float angle = transform.eulerAngles.z;
+
+        // Here we seperate the behavior into 3 states, based on the z angle
+        if (angle <= 10 || angle >= 350)
+        {
+            // Probably gonna have to add stuff here to make it feel right, but for now don't add vectoring
+
+        }
+        else if (angle > 10 && angle <= 180)
+        {
+
+            // squishify the angle to a reasonable value to be used as a scalar 
+            angle = Sigmoid90(angle);
+            velocity += (angle * transform.up) * speed * Time.deltaTime;
+
+        }
+        else if (angle > 180 && angle < 350)
+        {
+
+            // squishify the angle to a reasonable value to be used as a scalar 
+            angle = Sigmoid270(angle);
+            velocity += (angle * transform.up) * speed * Time.deltaTime;
+        }
+
+        // Now we add some direct stick input to give the kiteship some more control.
         // Check to see if there's no input, if there isn't then coast to 0, otherwise move.
         if ((leftStickVector.x + rightStickVector.x + leftStickVector.y + rightStickVector.y) == 0)
         {
@@ -65,17 +99,6 @@ public class KiteMovement : MonoBehaviour
             lastStickVector = leftStickVector + rightStickVector;
         }
 
-        // Create a rotation vector based off stick position difference and rotate based off it.
-        float stickOffset = (leftStickVector.y - rightStickVector.y) * 2;
-        Vector3 rotationVector = new Vector3(0, 0, -stickOffset);
-        transform.Rotate(rotationVector, Space.World);
-
-        // Now we need to apply 'upwards' velocity to the kiteship, with increasing velocity the more upside down it is.
-        float angle = transform.eulerAngles.z;
-        //velocity += new Vector3(0, angle, 0);
-        print(transform.eulerAngles.z);
-        //new Vector3(0, rotationVector.z, 0) transform.rotation.z
-        // Quaternion.Euler(rotationVector) * Vector3.up * Time.deltaTime;
 
         // Clamp the kiteship from going too far away from the orign.
         if (velocity.magnitude >= 20)
@@ -84,5 +107,18 @@ public class KiteMovement : MonoBehaviour
         }
 
         transform.position = velocity;
+    }
+
+    // These functions are far from perfect, but they squashes the euler rotation we get between 0 and 3, precious. Sigmoidin' cetner at a values of 90 / 180.
+    // See the wikipedia on logistic function / sigmoid function for getting what the variables here do.
+    public static float Sigmoid90(float value)
+    {
+        float k = -.1f * (value - 90); 
+        return 5 / (1 + Mathf.Exp(k));
+    }
+    public static float Sigmoid270(float value)
+    {
+        float k = -.1f * -(value - 270);
+        return 5 / (1 + Mathf.Exp(k));
     }
 }
