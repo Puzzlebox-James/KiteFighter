0cade06ed9bbea08188f61d05c8003234bc598d8
James Hage <jahage@comcast.net>
2020-05-04 13:13:18 -0600

    broken leg
    bunch of lame stuff

--- Kite Fighter/Assets/Scripts/UIEvents.cs
+++ Kite Fighter/Assets/Scripts/UIEvents.cs
@@ -2,9 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIEvents : MonoBehaviour
 {
+    // Variables on the 'Button Handler' Component, along with some hacky shit
+    public GameObject mainCamera;
+    private Vector3 modeSelectionCameraPosition = new Vector3(28, 11.5f, 110);
+
+    private bool cameraMoving = false;
+
+
+    public GameObject titleCanvas;
+    public GameObject modeSelectionCanvas;
+
+    // hacky references to the stage images
+    public Slider stageSelectSlider;
+    private float stageSliderValue;
+
+    public Image stageImage;
+    public Sprite cityRoofsStageImage;
+    public Sprite chillHillsStageImage;
+    public Sprite redCanyonStageImage;
+
+    public void Start()
+    {
+        titleCanvas.SetActive(true);
+        modeSelectionCanvas.SetActive(false);
+    }
+
+
     public void FlightTrainingButton()
     {
         SceneManager.LoadScene("TrainingScene");
@@ -12,7 +39,9 @@ public class UIEvents : MonoBehaviour
 
     public void FightButtonClick()
     {
-        SceneManager.LoadScene("GameScene");
+        cameraMoving = true;
+
+        //SceneManager.LoadScene("GameScene");
     }
 
     public void ToggleWind(bool value)
@@ -26,6 +55,50 @@ public class UIEvents : MonoBehaviour
             KiteMovement.windOn = false;
         }
     }
+
+
+    public void StageSelectSlider()
+    {
+        stageSliderValue = stageSelectSlider.GetComponent<Slider>().value;
+        if (stageSliderValue == 0)
+        {
+            Debug.Log("0");
+            stageImage.sprite = redCanyonStageImage;
+
+        } else if (stageSliderValue == 1)
+        {
+            Debug.Log("1");
+            stageImage.sprite = chillHillsStageImage;
+        }
+        else
+        {
+            Debug.Log("2");
+            stageImage.sprite = cityRoofsStageImage;
+        }
+    }
+
+    public void Update()
+    {
+        //mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        Vector3 faceDic = new Vector3(1, 0, 0);
+        Quaternion modeSelectionCameraRotation = Quaternion.LookRotation(faceDic, Vector3.up);
+
+        if (cameraMoving == true)
+        {
+            titleCanvas.SetActive(false);
+            modeSelectionCanvas.SetActive(true);
+            
+            mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, modeSelectionCameraPosition, 2 * Time.deltaTime);
+            mainCamera.transform.rotation = Quaternion.Lerp(mainCamera.transform.rotation, modeSelectionCameraRotation, 2 * Time.deltaTime);
+            
+            if (mainCamera.transform.position == modeSelectionCameraPosition)
+            {
+                cameraMoving = false;
+            }
+        }
+    }
+
+
 
     public void Exit()
     {
